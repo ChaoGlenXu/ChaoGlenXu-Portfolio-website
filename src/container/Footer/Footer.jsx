045b56d77865888a21119da446968efacfc53406
chao <chao@chaos-MacBook-Air.local>
2025-09-18 04:33:37 -0700

    updated my email

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -44,7 +44,7 @@ const Footer = () => {
       <div className="app__footer-cards">
         <div className="app__footer-card">
           <img src={images.email} alt="email" />
-          <a href="mailto:cgx7294@gmail.com" className="p-text"> cgx7294@gmail.com </a>
+          <a href="mailto:chaoglen.xu@alumni.utoronto.ca" className="p-text"> chaoglen.xu@alumni.utoronto.ca </a>
         </div>
 
         <div className="app__footer-card">
